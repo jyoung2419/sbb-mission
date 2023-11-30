2b056a9ddba0acf7ba13e67073fc9be9d19626ca
Jeong jinyoung <jjyoung2419@gmail.com>
2023-11-30 16:09:48 +0900

    2-01 ~ 2-03

--- src/main/java/com/ll/sbbmission/SbbApplication.java
+++ src/main/java/com/ll/sbbmission/SbbApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SbbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SbbMissionApplication.class, args);
+		SpringApplication.run(SbbApplication.class, args);
 	}
-
 }
