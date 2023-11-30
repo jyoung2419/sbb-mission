9bc9ba2e444939f331b11b21049d830c1b73fd5e
Jeong jinyoung <jjyoung2419@gmail.com>
2023-11-30 21:03:04 +0900

    3-02

--- src/main/java/com/ll/sbb/question/QuestionRepository.java
+++ src/main/java/com/ll/sbb/question/QuestionRepository.java
@@ -1,6 +1,7 @@
 package com.ll.sbb.question;
 
-import com.ll.sbb.question.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +10,6 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
     Question findBySubjectAndContent(String subject, String content);
     List<Question> findBySubjectLike(String subject);
+    Page<Question> findAll(Pageable pageable);
 
 }
